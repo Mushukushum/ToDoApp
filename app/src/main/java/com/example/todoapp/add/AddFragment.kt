51403b6b7caf0e4ced0df2669ec6eb50b57d12d0
Mushukushum <dmytromuzychuk2710@gmail.com>
2022-01-15 21:04:53 +0200

    Added sharedviewmodel, colors for priorities in add fragment

--- app/src/main/java/com/example/todoapp/add/AddFragment.kt
+++ app/src/main/java/com/example/todoapp/add/AddFragment.kt
@@ -1,23 +1,67 @@
 package com.example.todoapp.add
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.todoapp.R
+import com.example.todoapp.SharedViewModel
+import com.example.todoapp.data.models.Priority
+import com.example.todoapp.data.models.ToDoData
+import com.example.todoapp.data.viewmodel.ToDoViewModel
+import kotlinx.android.synthetic.main.fragment_add.*
+import kotlinx.android.synthetic.main.fragment_add.view.*
 
 class AddFragment : Fragment() {
+
+    private val mToDoViewModel: ToDoViewModel by viewModels()
+    private val mSharedViewModel: SharedViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view =  inflater.inflate(R.layout.fragment_add, container, false)
 
         setHasOptionsMenu(true)
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+
+        view.priorities.onItemSelectedListener = mSharedViewModel.listener
+
+        return view
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.add_fragment_menu, menu)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.menu_add){
+            insertDataToDatabase()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun insertDataToDatabase() {
+        val mTitle = title_edit_text.toString()
+        val mPriority = priorities.selectedItem.toString()
+        val mDescription = description_edit_text.text.toString()
+
+        val validation = mSharedViewModel.verifyDataFromUser(mTitle, mDescription)
+        if(validation){
+            val newData = ToDoData(
+                0,
+                mTitle,
+                mSharedViewModel.parsePriority(mPriority),
+                mDescription
+            )
+            mToDoViewModel.insertData(newData)
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        }else{
+            Toast.makeText(requireContext(), "Please, fill out all fields.", Toast.LENGTH_LONG).show()
+        }
+    }
+
 }
