fe6a2fd337933381868ea5cd21bdfe710ef9d4c9
Mushukushum <dmytromuzychuk2710@gmail.com>
2022-01-19 23:49:10 +0200

    Implemented DiffUtil

--- app/src/main/java/com/example/todoapp/list/adapter/ListAdapter.kt
+++ app/src/main/java/com/example/todoapp/list/adapter/ListAdapter.kt
@@ -2,6 +2,7 @@ package com.example.todoapp.list.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todoapp.data.models.ToDoData
 import com.example.todoapp.databinding.RowLayoutBinding
@@ -38,7 +39,10 @@ class ListAdapter: RecyclerView.Adapter<ListAdapter.MyViewHolder>(){
     }
 
     fun setData(toDoData: List<ToDoData>) {
+        val toDoDiffUtil = ToDoDiffUtil(dataList, toDoData)
+        val toDoDiffResult = DiffUtil.calculateDiff(toDoDiffUtil)
         this.dataList = toDoData
-        notifyDataSetChanged()
+        toDoDiffResult.dispatchUpdatesTo(this)
+
     }
 }
